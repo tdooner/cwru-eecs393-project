248b6b604211962390f810a6a9d1f794a1fc645f
Tom Dooner <tom@causes.com>
2012-10-06 01:50:19 -0400

    New Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieGameEngine::Application.config.secret_token = 'fc29659495e631dc3807e6fe812ec358d1d8cd21e4893a0a77a0aa011e769d1adb2b035615edc797ca783271c8804b438ff2b03baa8ba6e1bd13f6530ca1a797'
