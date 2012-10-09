521c12eede0209485a31822d9d059381c0f8bb55
Tom Dooner <tom@causes.com>
2012-10-09 15:35:27 -0400

    Add Engine call to get all players

--- lib/engine/plugins/donut_plugin.rb
+++ lib/engine/plugins/donut_plugin.rb
@@ -1,7 +1,12 @@
 class DonutPlugin < ZombieGame::Plugin
   filter_attribute Player, :name, :invert_display_name
+  filter_attribute Player, :caseid, :everyone_is_tom!
 
   def self.invert_display_name(name)
     "Not #{name}"
+  end
+
+  def self.everyone_is_tom!(caseid)
+    return 'ted27'
   end
 end
