6aded89f316a00169acad55a02c6e3eed2489400
Tom Dooner <tom@causes.com>
2012-11-17 21:24:31 -0500

    Hook up unregistering for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,34 @@
 class GamesController < ActionController::Base
   layout 'application'
+  before_filter :set_game
 
-  def show
-    @game = Game.find(params[:id])
-  end
+  def show; end
 
   def register
-    @game = Game.find(params[:id])
-    @user = current_user
-
-    if @user.registered?(@game)
+    if current_user.registered?(@game)
       flash[:notice] = 'You are already registered!'
     else
-      @user.register_for_game(@game)
+      current_user.register_for_game(@game)
       flash[:notice] = 'You have been registered!'
     end
+
     redirect_to game_url(@game)
   end
 
   def unregister
+    if current_user.registered?(@game)
+      current_user.unregister_for_game(@game)
+      flash[:notice] = 'You have been unregistered!'
+    else
+      flash[:notice] = 'You are not registered!'
+    end
 
+    redirect_to game_url(@game)
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
