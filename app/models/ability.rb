9501ce6c71ef3d39de07367ef1b26588df248e47
icd3 <git@iandimayuga.com>
2012-11-20 22:53:25 +0000

    Simplify ability definitions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   def initialize(user)
 
-    # Users can manage themselves
+    # Users can manage themselves but no one else
+    cannot :manage, User
+    cannot :manage, Player
     can :manage, User, :id => user.id
     can :manage, Player, :user_id => user.id
 
@@ -17,16 +19,16 @@ class Ability
       user.players.find(:game_id => game.id, :game_admin => true).any?
     end
 
+    # Only site admins can adminify people
+    cannot :adminify, [User, Player]
+
+    # Only site admins can create or destroy games
+    cannot [:create, :destroy], Game
+
     if user.site_admin?
       # Site admins can manage all attributes of users, players, games, squads
       can :manage, [User, Player, Game, Squad]
       can :read, Waiver
-    else
-      # Only site admins can adminify people
-      cannot :adminify, [User, Player]
-
-      # Only site admins can create or destroy games
-      cannot [:create, :destroy], Game
     end
   end
 end
