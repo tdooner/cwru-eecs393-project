991c2c6f4c484b60f7ea8205dfe10664c164ca9e
icd3 <git@iandimayuga.com>
2012-12-03 04:35:22 +0000

    Define user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  before_filter :set_user
+
+  def show
+    if cannot? :read, @user
+      flash[:error] = "You do not have permission to view that user's profile."
+      return redirect_to show_user_url(@current_user)
+    end
+
+    @players = @user.players
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
