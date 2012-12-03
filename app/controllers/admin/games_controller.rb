3c69569f712b0dfe7f5505812796ceb4a9733ad2
Tom Dooner <tom@causes.com>
2012-12-02 22:51:07 -0500

    Integrate Rafael's "cosmetic work"
    ...along with a couple revisions from myself.

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,35 +1,53 @@
 class Admin::GamesController < ApplicationController
-    def show
-        @game = Game.find(params[:id])
-        redirect_to(edit_admin_game_url(@game))
-    end
-    def index
-      @games = Game.all
-    end
-    def edit
-      @game = Game.find(params[:id])
-    end
-    def update
-      @game = Game.find(params[:id])
-      #@game.name = params[:name]
-      @game.attributes = params[:game]
+  before_filter :set_game, :except => [:index, :new, :create]
+
+  def show
+    # Nothing here -- the admin probably wants to edit the game instead.
+    redirect_to(edit_admin_game_url(@game))
+  end
+
+  def index
+    @games = Game.all
+  end
+
+  def update
+    @game.attributes = params[:game]
+
+    begin
       @game.save(:safe => true)
       flash[:notice] = 'Saved!'
-      render :edit
+    rescue
+      flash[:error] = 'An error occurred! Could not save!'
     end
-    def new
-      @game = Game.new
-    end
-    def create
-      @game = Game.new
-      @game.attributes = params[:game]
-      @game.save(:safe => true)
-      flash[:notice] = 'Game Created!'
-      @games = Game.all
-      render :index
-    end
-    def players
-      @game = Game.find(params[:id])
-      @players = @game.players
-    end
+
+    render :edit
+  end
+
+  def players
+    @game = Game.find(params[:id])
+    @players = @game.players
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(params[:game])
+    @game.save(:safe => true)
+    flash[:notice] = 'Game Created!'
+    @games = Game.all
+
+    render :index
+  end
+
+  def players
+    @players = @game.players
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
 end
